6c698782bdbd317b0f9ea2d7b718fadb714a4e3b
sylvain thenault <sylvain.thenault@logilab.fr>
2009-02-13 16:21:41 +0100

    person workflow

--- schema.py
+++ schema.py
@@ -7,11 +7,11 @@ Person.add_relation(ObjectRelation('Comment', cardinality='1*', composite='objec
 Person.add_relation(ObjectRelation('Tag'), name='tags')
 Person.add_relation(SubjectRelation('File'), name='concerned_by')
 Person.add_relation(SubjectRelation('State', cardinality='1*'), name='in_state')
+Person.add_relation(ObjectRelation('TrInfo', cardinality='1*', composite='object'), name='wf_info_for')
 
 Task = import_erschema('Task')
 Task.add_relation(ObjectRelation('Comment', cardinality='1*', composite='object'), name='comments')
 Task.add_relation(SubjectRelation('Person'), name='todo_by')
-Task.add_relation(SubjectRelation('State', cardinality='1*'), name='in_state')
 
 Event = import_erschema('Event')
 Event.add_relation(ObjectRelation('Comment', cardinality='1*', composite='object'), name='comments')
@@ -27,6 +27,7 @@ class School(EntityType):
     use_email     = SubjectRelation('EmailAddress', composite='subject')
 
     has_studied_in = ObjectRelation('Person')
+
 
 class has_studied_in(RelationType):
     """used to indicate an estabishment where a person has been studying"""
