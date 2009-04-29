ffd351d3b8819f7a565404af798615cde93bb9d6
Nicolas Chauvat <nicolas.chauvat@logilab.fr>
2009-04-29 02:36:43 +0200

    [optim] rewrite request to allow use of cache

--- views/primary.py
+++ views/primary.py
@@ -3,9 +3,9 @@ from cubes.person.views import PersonPrimaryView as BasePersonPrimaryView
 class PersonPrimaryView(BasePersonPrimaryView):
     
     def get_side_boxes_defs(self, entity):
-        rsets = []
+        boxes = []
         limit = self.req.property_value('navigation.related-limit') + 1
-        rql = 'Any E ORDERBY D DESC WHERE P use_email EA, E sender EA, E date D, P eid %s'
-        rset = self.req.execute(rql % entity.eid)
-        rsets.append( (_('recent email'), rset) )
-        return rsets
+        rql = 'Any E ORDERBY D DESC WHERE P use_email EA, E sender EA, E date D, P eid %(x)s'
+        rset = self.req.execute(rql, {'x': entity.eid})
+        boxes.append( (_('recent email'), rset) )
+        return boxes
