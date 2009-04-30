74c9439f0ceecf73240398eaccdfbe4aefde41b2
sylvain thenault <sylvain.thenault@logilab.fr>
2009-04-30 09:18:12 +0200

    turn a person primary view overriding into a pluggable section

--- views/primary.py
+++ views/primary.py
@@ -1,11 +1,16 @@
-from cubes.person.views import PersonPrimaryView as BasePersonPrimaryView
 
-class PersonPrimaryView(BasePersonPrimaryView):
-    
-    def get_side_boxes_defs(self, entity):
-        boxes = []
-        limit = self.req.property_value('navigation.related-limit') + 1
-        rql = 'Any E ORDERBY D DESC WHERE P use_email EA, E sender EA, E date D, P eid %(x)s'
-        rset = self.req.execute(rql, {'x': entity.eid})
-        boxes.append( (_('recent email'), rset) )
-        return boxes
+from cubicweb.selectors import implements, rql_condition
+from cubicweb.web.component import RelatedObjectsVComponent
+
+class SentMailVComponent(RelatedObjectsVComponent):
+    """email sent by this person"""
+    id = 'sentmail'
+    __select__ = implements('Person') & rql_condition('X use_email EA, E sender EA')
+    order = 40
+    # reuse generated message id
+    title = _('contentnavigation_sentmail')
+
+    def rql(self):
+        """override this method if you want to use a custom rql query"""
+        return 'Any E ORDERBY D DESC WHERE P use_email EA, E sender EA, E date D, P eid %(x)s'
+
