1d356c203ca2950e047895cce33906a4dbfa0519
sylvain thenault <sylvain.thenault@logilab.fr>
2009-05-12 21:05:46 +0200

    0.13

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -4,7 +4,7 @@
 distname = "cubicweb-drh"
 modname = distname.split('-', 1)[1]
 
-numversion = (0, 12, 3)
+numversion = (0, 13, 0)
 version = '.'.join(str(num) for num in numversion)
 
 license = 'LGPL'
