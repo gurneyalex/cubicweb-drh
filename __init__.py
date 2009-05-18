9b2580b786c38e388a19bc2e415194d8f530bc4d
Graziella Toutoungis <graziella.toutoungis@logilab.fr>
2009-05-18 12:24:37 +0200

    move tag t view + correct trans

--- __init__.py
+++ __init__.py
@@ -1,4 +1,1 @@
-from cubicweb.web import uicfg
 
-uicfg.actionbox_appearsin_addmenu.tag_object_of(('Person', 'concerned_by', '*'), True)
-uicfg.actionbox_appearsin_addmenu.tag_object_of(('Person', 'todo_by', '*'), True)
--- views/__init__.py
+++ views/__init__.py
@@ -3,7 +3,7 @@
 from cubicweb.web import uicfg
 
 uicfg.actionbox_appearsin_addmenu.tag_subject_of(('Person', 'concerned_by', '*'), True)
-uicfg.actionbox_appearsin_addmenu.tag_object_of(('*', 'todo_by', 'person'), True)
+uicfg.actionbox_appearsin_addmenu.tag_object_of(('*', 'todo_by', 'Person'), True)
 uicfg.actionbox_appearsin_addmenu.tag_subject_of(('School', 'filed_under', 'Folder'), False)
 uicfg.autoform_is_inlined.tag_subject_of(('School', 'use_email', '*'), True)
 uicfg.autoform_is_inlined.tag_subject_of(('School', 'phone', '*'), True)
