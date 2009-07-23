51fcd11fb1bef1c0afe374aac37b3538eaa22d25
Graziella Toutoungis <graziella.toutoungis@logilab.fr>
2009-07-23 14:14:27 +0200

    hide the concerned_by generated box in order to add a custom box add the person entry on the application's form

--- views/__init__.py
+++ views/__init__.py
@@ -7,3 +7,5 @@ uicfg.actionbox_appearsin_addmenu.tag_object_of(('*', 'todo_by', 'Person'), True
 uicfg.actionbox_appearsin_addmenu.tag_subject_of(('School', 'filed_under', 'Folder'), False)
 uicfg.autoform_is_inlined.tag_subject_of(('School', 'use_email', '*'), True)
 uicfg.autoform_is_inlined.tag_subject_of(('School', 'phone', '*'), True)
+uicfg.primaryview_section.tag_subject_of(('Person', 'concerned_by', '*'), 'hidden')
+uicfg.autoform_section.tag_subject_of(('Application', 'for_person', '*'), 'primary')
