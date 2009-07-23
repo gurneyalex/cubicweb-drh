e9939f494859bcac89a1a7ab9dbb0d10bf2b5fc0
Graziella Toutoungis <graziella.toutoungis@logilab.fr>
2009-07-23 14:07:58 +0200

    add apllication entity delete the state and address relation from Person

--- schema.py
+++ schema.py
@@ -1,13 +1,9 @@
-
 Person = import_erschema('Person')
-Person.add_relation(String(maxsize=512), name='address')
 Person.add_relation(Date(), name='birthday')
-
 Person.add_relation(ObjectRelation('Comment', cardinality='1*', composite='object'), name='comments')
 Person.add_relation(ObjectRelation('Tag'), name='tags')
 Person.add_relation(SubjectRelation('File'), name='concerned_by')
-Person.add_relation(SubjectRelation('State', cardinality='1*'), name='in_state')
-Person.add_relation(ObjectRelation('TrInfo', cardinality='1*', composite='object'), name='wf_info_for')
+
 
 Task = import_erschema('Task')
 Task.add_relation(ObjectRelation('Comment', cardinality='1*', composite='object'), name='comments')
@@ -15,6 +11,7 @@ Task.add_relation(SubjectRelation('Person'), name='todo_by')
 
 Event = import_erschema('Event')
 Event.add_relation(ObjectRelation('Comment', cardinality='1*', composite='object'), name='comments')
+
 
 class School(EntityType):
     """an (high) school"""
@@ -37,5 +34,12 @@ class has_studied_in(RelationType):
 class interested_in(RelationDefinition):
     subject = ('Person', 'CWUser')
     object = 'Event'
+
+
+class Application(WorkflowableEntityType):
+    for_person = SubjectRelation('Person', cardinality='*1')
+    date = Datetime(default='TODAY', required=True)
+
+
 
 
