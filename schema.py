798cdcd2bf42869a653baf4e693d72ee486916d4
Nicolas Chauvat <nicolas.chauvat@logilab.fr>
2009-08-15 00:15:46 +0200

    bugfixing

--- schema.py
+++ schema.py
@@ -49,7 +49,7 @@ class interested_in(RelationDefinition):
 class Application(WorkflowableEntityType):
     for_person = SubjectRelation('Person', cardinality='*1')
     date = Datetime(default='TODAY', required=True)
-
+    tags = ObjectRelation('Tag')
 
 
 
--- views/startup.py
+++ views/startup.py
@@ -19,12 +19,13 @@ class IndexView(startup.ManageView):
                    % (html_escape(self.build_url(rql=rql, vtitle=title)),
                       len(rset), title))
         # candidatures en attente
-        rset = self.req.execute('Any CD,P,group_concat(TN),E,B '
-                                'GROUPBY P,E,B,CD ORDERBY CD '
-                                'WHERE P in_state X, P is Person, '
-                                'X name "application", '
-                                'T? tags P, T name TN, P has_studied_in E?, '
-                                'P birthday B?, P creation_date CD')
+        rset = self.req.execute('Any A,P,group_concat(TN),E,B '
+                                'GROUPBY A,P,E,B,CD ORDERBY CD '
+                                'WHERE A is Application, A in_state X, '
+                                'X name "received", '
+                                'A for_person P, P has_studied_in E?, '
+                                'P birthday B?, T? tags A, T name TN, '
+                                'A creation_date CD')
         if rset:
             self.w(u'<h2>%s</h2>' % _('Juger candidatures'))
             self.wview('table',rset,'null')
