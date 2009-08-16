d023160eff80bc90b7a49554a08116a5bce64f17
Nicolas Chauvat <nicolas.chauvat@logilab.fr>
2009-08-16 13:56:29 +0200

    F improve workflow

--- migration/0.14.1_Any.py
+++
@@ -1,5 +0,0 @@
-# application are taggable
-add_relation_definition('Tag','tags','Application')
-# move tags from person to applications
-rql('SET T tags A WHERE T tags P, P is Person, A for_person P')
-rql('DELETE T tags P WHERE P is Person')
