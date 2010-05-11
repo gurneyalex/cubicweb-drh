a4e7647c58239313981d21d35b43e2d4abc20f36
Sylvain Th?nault <sylvain.thenault@logilab.fr>
2010-05-11 08:44:18 +0200

    setuptools support, pkginfo cleanups

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -8,19 +8,12 @@ numversion = (0, 18, 0)
 version = '.'.join(str(num) for num in numversion)
 
 license = 'LGPL'
-copyright = '''Copyright (c) 2003-2010 LOGILAB S.A. (Paris, FRANCE).
-http://www.logilab.fr/ -- mailto:contact@logilab.fr'''
 
 author = "Logilab"
 author_email = "contact@logilab.fr"
 
-short_desc = "recruitment application based on the CubicWeb framework"
-long_desc = """\
-This cube provides an human resources management application usable to manage a
-recruitment process.
-"""
+description = "recruitment application based on the CubicWeb framework"
 
-ftp = ''
 web = 'http://www.cubicweb.org/project/%s' % distname
 
 classifiers = [
@@ -30,43 +23,41 @@ classifiers = [
     'Programming Language :: JavaScript',
     ]
 
-pyversions = ['2.4']
+__depends__ = {'cubicweb': '>= 3.8.0',
+               'cubicweb-file': None,
+               'cubicweb-email': None,
+               'cubicweb-person': None,
+               'cubicweb-addressbook': None,
+               'cubicweb-folder': None,
+               'cubicweb-tag': None,
+               'cubicweb-comment': None,
+               # could be moved out
+               'cubicweb-basket': None,
+               'cubicweb-event': None,
+               'cubicweb-task': None,
+               }
 
-orig_dir = 'schemas'
+# packaging ###
 
-__depends_cubes__ = {'file': None,
-                     'email': None,
-                     'person': None,
-                     'addressbook': None,
-                     'folder': None,
-                     'tag': None,
-                     'comment': None,
-                     # could be moved out
-                     'basket': None,
-                     'event': None,
-                     'task': None,
-                     }
-__depends__ = {'cubicweb': '>= 3.8.0'}
-for key, value in __depends_cubes__.items():
-    __depends__['cubicweb-'+key] = value
-__use__ = tuple(__depends_cubes__)
+from os import listdir as _listdir
+from os.path import join, isdir, exists, dirname
+from glob import glob
 
-from os import listdir
-from os.path import join
+THIS_CUBE_DIR = join('share', 'cubicweb', 'cubes', modname)
 
-TEMPLATES_DIR = join('share', 'cubicweb', 'cubes')
-try:
-    data_files = [
-        [join(TEMPLATES_DIR, 'drh'),
-         [fname for fname in listdir('.')
-          if fname.endswith('.py') and fname != 'setup.py']],
-        [join(TEMPLATES_DIR, 'drh', 'views'),
-         [join('views', fname) for fname in listdir('views')]],
-        [join(TEMPLATES_DIR, 'drh', 'i18n'),
-         [join('i18n', fname) for fname in listdir('i18n')]],
-        [join(TEMPLATES_DIR, 'drh', 'migration'),
-         [join('migration', fname) for fname in listdir('migration')]],
-        ]
-except OSError:
-    # we are in an installed directory
-    pass
+def listdir(dirpath):
+    return [join(dirpath, fname) for fname in _listdir(dirpath)
+            if fname[0] != '.' and not fname.endswith('.pyc')
+            and not fname.endswith('~')
+            and not isdir(join(dirpath, fname))]
+
+data_files = [
+    # common files
+    [THIS_CUBE_DIR, [fname for fname in glob('*.py') if fname != 'setup.py']],
+    ]
+# check for possible extended cube layout
+for dirname in ('entities', 'views', 'sobjects', 'hooks', 'schema', 'data', 'i18n', 'migration'):
+    if isdir(dirname):
+        data_files.append([join(THIS_CUBE_DIR, dirname), listdir(dirname)])
+# Note: here, you'll need to add subdirectories if you want
+# them to be included in the debian package
