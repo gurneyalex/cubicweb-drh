e945072c9372b5013f703a0099c9f1ca2e08f824
Sylvain Th?nault <sylvain.thenault@logilab.fr>
2011-02-10 12:26:24 +0100

    0.18.1

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -4,18 +4,14 @@
 modname = 'drh'
 distname = "cubicweb-drh"
 
-numversion = (0, 18, 0)
+numversion = (0, 18, 1)
 version = '.'.join(str(num) for num in numversion)
 
 license = 'LGPL'
-
 author = "Logilab"
 author_email = "contact@logilab.fr"
-
 description = "recruitment application based on the CubicWeb framework"
-
 web = 'http://www.cubicweb.org/project/%s' % distname
-
 classifiers = [
     'Environment :: Web Environment',
     'Framework :: CubicWeb',
@@ -23,7 +19,7 @@ classifiers = [
     'Programming Language :: JavaScript',
     ]
 
-__depends__ = {'cubicweb': '>= 3.8.0',
+__depends__ = {'cubicweb': '>= 3.10.0',
                'cubicweb-file': None,
                'cubicweb-email': None,
                'cubicweb-person': None,
@@ -40,7 +36,7 @@ __depends__ = {'cubicweb': '>= 3.8.0',
 # packaging ###
 
 from os import listdir as _listdir
-from os.path import join, isdir, exists, dirname
+from os.path import join, isdir
 from glob import glob
 
 THIS_CUBE_DIR = join('share', 'cubicweb', 'cubes', modname)
@@ -56,7 +52,7 @@ data_files = [
     [THIS_CUBE_DIR, [fname for fname in glob('*.py') if fname != 'setup.py']],
     ]
 # check for possible extended cube layout
-for dirname in ('entities', 'views', 'sobjects', 'hooks', 'schema', 'data', 'i18n', 'migration'):
+for dirname in ('entities', 'views', 'sobjects', 'hooks', 'schema', 'data', 'i18n', 'migration', 'wdoc'):
     if isdir(dirname):
         data_files.append([join(THIS_CUBE_DIR, dirname), listdir(dirname)])
 # Note: here, you'll need to add subdirectories if you want
