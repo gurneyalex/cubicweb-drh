1fce8b9bbf1c43d21c7d5f8f3d37a901eb3e90f3
Aurelien Campeas <aurelien.campeas@logilab.fr>
2014-09-12 12:18:19 +0200

    [entities] fetch_order -> cw_fetch_order

--- entities.py
+++ entities.py
@@ -4,7 +4,7 @@ from cubicweb.entities.adapters import ITreeAdapter
 
 class School(AnyEntity):
     __regid__ = 'School'
-    fetch_attrs, fetch_order = fetch_config(['name'])
+    fetch_attrs, cw_fetch_order = fetch_config(['name'])
 
 
 class EmailThreadITreeAdapter(ITreeAdapter):
